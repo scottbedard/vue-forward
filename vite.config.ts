bf2a4be5e82ee27f8c778295144fa5e73cc86ff4
Scott Bedard <scottbedard90@gmail.com>
2024-05-22 22:41:58 -0500

    exclude vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       name: 'VueForward',
     },
     rollupOptions: {
-      external: ['vue', 'vuex'],
+      external: ['vue', 'vue-router', 'vuex'],
       output: {
         globals: {
           vue: 'Vue',
