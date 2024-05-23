0823c06d92dc356165089bd1f8956dcc42b2cc29
Scott Bedard <scottbedard90@gmail.com>
2024-05-22 22:08:01 -0500

    parse json

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -2,18 +2,18 @@ import { execSync as exec } from 'child_process'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 import path from 'path'
-import pkg from '../package.json' with { type: 'json' }
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const index = path.resolve(__dirname, '../src/index.ts')
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')))
 
 async function cli() {
   const src = String(fs.readFileSync(index)).replace('x.y.z', pkg.version)
 
   fs.writeFileSync(index, src)
 
-  exec('pnpm build && pnpm publish')
+  exec('pnpm build && pnpm publish --access public')
 }
 
 cli()
